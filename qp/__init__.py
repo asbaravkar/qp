f24bade73b1dd47f99c5e2da0e73cb11955ee797
Aditya <aditya.baravkar@gmail.com>
2021-04-19 17:21:47 +0530

    Starting db (models)

--- qp/__init__.py
+++ qp/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from qp.config import Config
 
+db = SQLAlchemy(app)
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
     from qp.main.routes import main
     from qp.errors.handlers import errors
     app.register_blueprint(main)
