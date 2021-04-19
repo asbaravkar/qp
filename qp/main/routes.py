7faa5fdfb9dd769efc83bc19de935f5fc9de6e7f
Aditya <aditya.baravkar@gmail.com>
2021-04-19 16:20:20 +0530

    Adding form

--- qp/main/routes.py
+++ qp/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 
 
 main = Blueprint('main', __name__)
@@ -18,3 +18,12 @@ def about_page():
 @main.route('/contact')
 def contact_page():
     return render_template('contact.html')
+
+@main.route('/result', methods=['POST', 'GET'])
+def result_page():
+    if request.method == 'POST':
+        course = request.form.get('course')
+        branch = request.form.get('branch')
+        sem = request.form.get('semester')
+        form_data = [course, branch, sem]
+        return render_template('result.html', form=form_data)
